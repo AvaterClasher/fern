feeae07e3381d9f419af7dada4b2c083196c976a
AvaterClasher <avater.clasher@gmail.com>
2026-01-18 14:16:01 +0530

    refactor(hono): simplify HonoServiceDeclarationReferencer to use PackageId
    Simplify HonoServiceDeclarationReferencer to match Express pattern:
    - Use PackageId instead of complex {serviceId, service} structure
    - Resolve service names through packageResolver
    - Handle root service case explicitly
    - Fix 'pascalCase' property access error
    
    This aligns with the Express generator architecture and fixes
    compilation errors related to DeclaredServiceName structure.

--- generators/typescript/hono/generator/src/declaration-referencers/HonoServiceDeclarationReferencer.ts
+++ generators/typescript/hono/generator/src/declaration-referencers/HonoServiceDeclarationReferencer.ts
@@ -1,22 +1,10 @@
-import { HttpService, ServiceId } from "@fern-fern/ir-sdk/api";
 import { ExportedFilePath, PackageId, Reference } from "@fern-typescript/commons";
 
 import { AbstractHonoServiceDeclarationReferencer } from "./AbstractHonoServiceDeclarationReferencer";
 import { DeclarationReferencer } from "./DeclarationReferencer";
 
-export declare namespace HonoServiceDeclarationReferencer {
-    export interface Init extends AbstractHonoServiceDeclarationReferencer.Init {}
-}
-
-export class HonoServiceDeclarationReferencer extends AbstractHonoServiceDeclarationReferencer<{
-    serviceId: ServiceId;
-    service: HttpService;
-}> {
-    constructor(init: HonoServiceDeclarationReferencer.Init) {
-        super(init);
-    }
-
-    public getExportedFilepath(name: { serviceId: ServiceId; service: HttpService }): ExportedFilePath {
+export class HonoServiceDeclarationReferencer extends AbstractHonoServiceDeclarationReferencer<PackageId> {
+    public getExportedFilepath(name: PackageId): ExportedFilePath {
         return {
             directories: [...this.getExportedDirectory(name)],
             file: {
@@ -25,22 +13,23 @@ export class HonoServiceDeclarationReferencer extends AbstractHonoServiceDeclara
         };
     }
 
-    public getFilename(name: { serviceId: ServiceId; service: HttpService }): string {
+    public getFilename(name: PackageId): string {
         return `${this.getExportedNameOfService(name)}.ts`;
     }
 
-    public getExportedNameOfService(name: { serviceId: ServiceId; service: HttpService }): string {
-        return `${name.service.name.pascalCase.safeName}Service`;
+    public getExportedNameOfService(name: PackageId): string {
+        if (name.isRoot) {
+            return "RootService";
+        }
+        const subpackage = this.packageResolver.resolveSubpackage(name.subpackageId);
+        return `${subpackage.name.pascalCase.unsafeName}Service`;
     }
 
-    public getReferenceToService(args: DeclarationReferencer.getReferenceTo.Options<{
-        serviceId: ServiceId;
-        service: HttpService;
-    }>): Reference {
+    public getReferenceToService(args: DeclarationReferencer.getReferenceTo.Options<PackageId>): Reference {
         return this.getReferenceTo(this.getExportedNameOfService(args.name), args);
     }
 
-    protected getPackageIdFromName(name: { serviceId: ServiceId; service: HttpService }): PackageId {
-        return name.service.name.fernFilepath.packagePath;
+    protected getPackageIdFromName(packageId: PackageId): PackageId {
+        return packageId;
     }
 }
